0c92df8ee191bdbfe71517fb82c5d151bda63190
Joha Kim <johakim9936@gmail.com>
2026-08-23 11:18:57 -0400

    implement basic table using native html elements

--- src/data-table/DataTable.tsx
+++ src/data-table/DataTable.tsx
@@ -16,12 +16,61 @@
  * - Make sort and filter work together (filter first, then sort the filtered results).
  */
 
+import { useEffect, useState } from "react";
+
 import styles from "./DataTable.module.css";
 
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  company: { name: string };
+}
+
 export const DataTable = () => {
+  const [users, setUsers] = useState<Array<User>>([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users?limit=30")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.users);
+      });
+  }, []);
+
+  const isTableRendered = users.length > 0;
+
   return (
     <div>
       <p>2. Build a data table with sorting and filtering.</p>
+      {isTableRendered && (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Email</th>
+              <th>Company</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td>
+                    {user.firstName} {user.lastName}
+                  </td>
+                  <td>{user.age}</td>
+                  <td>{user.email}</td>
+                  <td>{user.company.name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
